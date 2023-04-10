213236b3a8126083424361c73c036ba4f28e4fc9
josellia <joseliaapires@hotmail.com>
2023-04-10 18:59:04 -0300

    chore: versão dos micros

--- packages/root/src/deva-root-config.ts
+++ packages/root/src/deva-root-config.ts
@@ -9,23 +9,23 @@ registerApplication({
   activeWhen: ["/"],
 });
 
-registerApplication({
-  name: "@deva/app-footer",
-  app: () =>
-    System.import<LifeCycles>(
-      "@deva/app-footer"
-    ),
-  activeWhen: ["/"],
-});
+// registerApplication({
+//   name: "@deva/app-footer",
+//   app: () =>
+//     System.import<LifeCycles>(
+//       "@deva/app-footer"
+//     ),
+//   activeWhen: ["/"],
+// });
 
-registerApplication({
-  name: "@deva/app-shop",
-  app: () =>
-    System.import<LifeCycles>(
-      "@deva/app-shop"
-    ),
-  activeWhen: ["/app1"],
-});
+// registerApplication({
+//   name: "@deva/app-shop",
+//   app: () =>
+//     System.import<LifeCycles>(
+//       "@deva/app-shop"
+//     ),
+//   activeWhen: ["/app1"],
+// });
 
 // registerApplication({
 //   name: "@deva/navbar",
